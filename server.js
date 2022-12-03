fd4f64047d600e8a98a7902e9c680e85aadd1a6c
Ashwini-tec <ashiwni@ariantechsolutions.com>
2022-12-03 18:17:55 +0530

    feature: setup swagger documentation

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 import express from 'express';
 import bodyparser from 'body-parser';
 import * as routes from './src/index.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+
+/**
+ * swagger docs
+ */
+app.use(
+    '/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(YAML.load(path.resolve() + '/api-docs.yml'))
+);
+
 
 /**
  * adding routes
